401af80131f5fafdc6615a9dc66e6547a198f74b
steve-the-hermit <stevemacharia309@gmail.com>
2023-09-21 23:14:17 +0300

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,53 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    def is_sentence(self):
+        if self.value and self.value.endswith("."):
+            return True
+        return False
+
+    def is_question(self):
+        if self.value and self.value.endswith("?"):
+            return True
+        return False
+
+    def is_exclamation(self):
+        if self.value and self.value.endswith("!"):
+            return True
+        return False
+
+    def count_sentences(self):
+        sentences = self.value.split('.')
+        sentences = [s for s in sentences if s]  # Remove empty strings
+        return len(sentences)
+
+# Testing the MyString class
+if __name__ == "__main__":
+    # Create an instance of MyString
+    my_string = MyString()
+
+    # Test is_sentence method
+    print(my_string.is_sentence())  # Should print False
+
+    # Test is_question method
+    print(my_string.is_question())  # Should print False
+
+    # Test is_exclamation method
+    print(my_string.is_exclamation())  # Should print False
+
+    # Test count_sentences method
+    print(my_string.count_sentences())  # Should print 0
+
+    # Update the value
+    my_string.value = "Hello World."
+    print(my_string.is_sentence())  # Should print True
+
+    my_string.value = "How are you?"
+    print(my_string.is_question())  # Should print True
+
+    my_string.value = "Great job!"
+    print(my_string.is_exclamation())  # Should print True
+
+    my_string.value = "This is a test. This is a test too."
+    print(my_string.count_sentences())  # Should print 2
